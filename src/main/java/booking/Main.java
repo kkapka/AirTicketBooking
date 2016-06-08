a9d8af600b0c438a8aac16cd8f0374599427d1dc
kkapka <kamil.kapka@hotmail.com>
2016-06-08 16:29:00 +0200

    ready?

--- src/main/java/booking/Main.java
+++ src/main/java/booking/Main.java
@@ -4,9 +4,6 @@ import java.io.FileNotFoundException;
 
 public class Main {
 	public static void main(String[] argv) throws FileNotFoundException {		
-		Database db=Database.getInstance();
-		
-		System.out.println(db.getAirportsList().size());
 		
 		SearchFlightView view=new SearchFlightView();
 		SearchFlightModel model=new SearchFlightModel();
